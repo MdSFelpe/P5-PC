db92cfbaac1a119f670b26717afad45b5c4d8778
Felpe <fellypeaugusto04@gmail.com>
2024-05-20 15:31:48 -0300

    Segunda versão

--- Login.js
+++ Login.js
@@ -1,4 +1,15 @@
-const form =document.getElementById("form");
+/* const form =document.getElementById("form");
 const input_email = document.getElementsByClassName("input-email");
 const input_senha = document.getElementsByClassName("input-email");
-const buton = 
+const buton = document.getElementsByClassName("buton");
+form.addEventListener ('submit', (event)=> {
+    event.preventDefault();
+    CheckForm();
+  
+})
+
+
+function redirectToCadastro() {
+    window.location.href = 'Cadastro.html'; 
+}
+ */
